6677d1a7bb3a689159aa534f74e6f206d606f67d
Faina <fadoms@taltech.ee>
2024-11-15 14:25:22 +0200

    about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Welcome to Read It Later – your personal reading wishlist. </h1>
+    <p>Here, you can easily add books to your reading list, ensuring you never miss a title you’re interested in exploring.</p>
+    <p>Discover a world of books waiting to be read with Read It Later. Save any book that catches your interest and find books that match your favorite genres.</p>
+    <p>Whether you’re creating a must-read list or searching for your next unforgettable story, Read It Later makes it simple to explore and dive into the world of books you love.</p>
+    <p>Start your reading adventure today!</p>
   </div>
 </template>
 
 <style>
+.about {
+  margin: 4vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+h1 {
+  align-self: center;
+  font-size: 24px;
+  margin-bottom: 2vw;
+}
+
+p {
+  margin-bottom: 1vw;
+}
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+
+
   }
 }
 </style>
+<script setup>
+</script>
